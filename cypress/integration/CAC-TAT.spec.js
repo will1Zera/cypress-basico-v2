59aeb357b52e2aa249ea247d5b34b3870be82cfc
William Bierhals <willambierhals>
2023-09-12 14:40:36 -0300

    :books: Testes das sessões 4, 5, 6 e 7 do curso

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -46,7 +46,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.fillFieldAndValidate('#lastName', 'Bierhals');
         cy.fillFieldAndValidate('#email', 'william#gmail.com');
         cy.fillFieldAndValidate('#open-text-area', 'Estou com uma dúvida');  
-        cy.get('#phone-checkbox').click();
+        cy.get('#phone-checkbox').check();
         cy.get('.button').click();
         cy.get('.error').should('be.visible');
     });
@@ -62,5 +62,44 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('.button').click();
         cy.get('.error').should('be.visible');
     });
+
+    it('seleciona um produto (YouTube) por seu texto', function() { 
+        cy.get('#product').select('YouTube').should('have.value', 'youtube');
+    });
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function() { 
+        cy.get('#product').select('mentoria').should('have.value', 'mentoria');
+    });
+
+    it('seleciona um produto (Blog) por seu índice', function() { 
+        cy.get('#product').select(1).should('have.value', 'blog');
+    });
+
+    it('seleciona o tipo de atendimento "Feedback', function() { 
+        cy.get('input[value="ajuda"]').check().should('have.value', 'ajuda');
+    });
+
+    it('marca cada tipo de atendimento', function() { 
+        cy.get('input[type="radio"]').should('have.length', 3).
+        each(function($radio) {
+            cy.wrap($radio).check();
+            cy.wrap($radio).should('be.checked');
+        });
+    });
+
+    it('marca ambos checkboxes, depois desmarca o último', function() { 
+        cy.get('#check input[type="checkbox"]').check().last().uncheck().should('not.be.checked');
+    });
+
+    it('seleciona um arquivo da pasta fixtures', function() { 
+        cy.get('#file-upload').selectFile('cypress/fixtures/example.json').
+        then(input => {expect(input[0].files[0].name).to.equal('example.json');});
+    });
+
+    it('seleciona um arquivo simulando um drag-and-drop', function() { 
+        cy.get('#file-upload').selectFile('cypress/fixtures/example.json', {action:'drag-drop'}).
+        then(input => {expect(input[0].files[0].name).to.equal('example.json');});
+    });
+    
 });
   
